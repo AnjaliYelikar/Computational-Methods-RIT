7b13f0127f58da55c932dd81696f4ea08a810d39
Anjali Balasaheb Yelikar <anjali.yelikar@ligo.org>
2020-09-10 17:00:31 -0400

    Matrix class added

--- HW2/Q3/Matrix_class.py
+++ HW2/Q3/Matrix_class.py
@@ -1,23 +1,40 @@
 
-class Matrix(object):
+class Matrix:
+    def __init__(self, matrixA, matrixB):
+        self.A = matrixA
+        self.B = matrixB
+        
+    def __addition__(self,other):
+        C = np.zeros((len(A),len(A[0]))
+                     
+        for i in range(len(A)):    #rows
+            for j in range(len(A[0])): #columns
+                C[i][j] = A[i][j] + B[i][j]
+                  
+	def __multiplication(self,other):
+		C = np.zeros((len(A),len(A[0]))) 
 
-    def __init__(self, grid):
-        self.g = grid
-        self.h = len(grid)
-        self.w = len(grid[0])
-    
-    def __add__(self,other):
+		for i in range(len(A)):                 # iterate through rows of X
+		     for j in range(len(B[0])):           # iterate through columns of Y
+		         for k in range(len(B)):          # iterate through rows of Y
+        		    C[i][j] += A[i][k] * B[k][j]
 
-        if self.h != other.h or self.w != other.w:
-            raise(ValueError, "Matrices can only be added if the dimensions are the same") 
 
-        matrix_addition = []
-        for i in range(self.h):
-            new_row = []
-            for j in range(self.w):
-                addition = self.g[i][j] + other.g[i][j]
-                new_row.append(addition)
+	def __transpose(self):
+		A_T =  np.zeros((len(A[0]),len(A))) 
+		
+		for i in range(len(A)):
+			 for j in range(len(A[0])):
+     			A_T[j][i] = A[i][j]
 
-            matrix_addition.append(new_row)
+	
+	def __trace(self):
+		trace = 0.
+		for i in range(len(A)):
+			trace += A[i][i]
 
-        return Matrix(matrix_addition)
+#A = [[1,2,3],[1,1,1]]
+#B = [[1,2,1],[1,1,2]]
+                     
+#f = Matrix(A,B)
+#print(f.addition())
